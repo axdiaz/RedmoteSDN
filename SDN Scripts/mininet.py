93ae15ca74eccbbb440ac17ed70306775658025f
Erick Ceja Fuentes <erick@pixan.io>
2019-07-30 22:14:32 -0500

    added controller options

--- SDN Scripts/mininet.py
+++ SDN Scripts/mininet.py
@@ -17,12 +17,12 @@ def myNetwork():
                   build=False,
                   ipBase='10.0.0.0/8')
 
-    info('*** Adding controller\n')
-    c0 = net.addController(name='c0',
-                           controller='ryu',
-                           ip='127.0.0.1',
-                           protocol='tcp',
-                           port=6633)
+    # info('*** Adding controller\n')
+    # c0 = net.addController(name='c0',
+    #                        controller='ryu',
+    #                        ip='127.0.0.1',
+    #                        protocol='tcp',
+    #                        port=6633)
 
     info('*** Add switches\n')
     s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
@@ -40,17 +40,17 @@ def myNetwork():
     info('*** Starting network\n')
     net.build()
 
-    info('*** Starting controllers\n')
-    for controller in net.controllers:
-        controller.start()
+    # info('*** Starting controllers\n')
+    # for controller in net.controllers:
+    #     controller.start()
 
-    info('*** Starting switches\n')
-    net.get('s1').start([c0])
-    net.get('s2').start([c0])
+    # info('*** Starting switches\n')
+    # net.get('s1').start([c0])
+    # net.get('s2').start([c0])
 
-    info('*** Post configure switches and hosts\n')
-    net.pingAll()
-    net.stop()
+    # info('*** Post configure switches and hosts\n')
+    # net.pingAll()
+    # net.stop()
 
 
 if __name__ == '__main__':
