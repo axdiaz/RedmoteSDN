4e2c2cbdf8d6599148963bc844e2645aa0d28331
erickcejaf <erick-13579@hotmail.com>
2019-07-30 21:34:59 -0700

    Infraestructure and settings needed for production

--- RedmoteWEB/RedmoteWEB/wsgi.py
+++ RedmoteWEB/RedmoteWEB/wsgi.py
@@ -7,10 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
-from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/var/www/html/RedmoteSDN/RedmoteWEB/RedmoteWEB/')
+
+sys.path.append('/var/www/html/RedmoteSDN/RedmoteWEB/linux-venv/lib/python3.6/site-packages')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RedmoteWEB.settings')
+
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
